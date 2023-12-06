2cebc3cb565f11fb7d7596e352f01e6172f384bd
Danit <danit120@gmail.com>
2023-12-07 00:50:45 +0200

    meeting db is working but need to double check, validation is missing

--- drafts.js
+++ drafts.js
@@ -27,31 +27,19 @@ function toggleModal() {
             id: id,
             firstName: firstName,
             lastName: lastName,
-            mobile: mobile
-        });
-        
-        localStorage.setItem("guests", JSON.stringify(existingGuests));
-
-        var newMeeting = {
-            meetingId : "1",
+            mobile: mobile,
             building: building,
             floor: floor,
-            host: host,
-            guests: existingGuests
-        }
+            host: host
 
-        console.log(newMeeting);
+        });
 
-        // Get existing meetings or initialize an empty array
-        var existingMeetings = JSON.parse(localStorage.getItem("meetings")) || [];
+        localStorage.setItem("guests", JSON.stringify(existingGuests));
 
-        // Add the new meeting details
-        existingMeetings.push(newMeeting);
-
-        // Save the updated meetings array to local storage
-        localStorage.setItem("meetings", JSON.stringify(existingMeetings));
-
-
+        location.reload();
+    }
+    
+    function loadGuestDetails() {
         var detailsRow = document.getElementById("detailsRow");
         if (detailsRow) {
             detailsRow.remove();
@@ -60,7 +48,8 @@ function toggleModal() {
         var existingGuests = JSON.parse(localStorage.getItem("guests")) || [];
         var tableBody = document.getElementById("guest-table");
     
-  
+        existingGuests.forEach(guest => {
+           // console.log("Guest Details:", guest);
             var row = tableBody.insertRow();
             var cell0 = row.insertCell(0);
             var cell1 = row.insertCell(1);
@@ -69,21 +58,39 @@ function toggleModal() {
             var cell4 = row.insertCell(4);
 
             cell0.textContent ="";
-            cell1.textContent = id;
-            cell2.textContent = firstName;
-            cell3.textContent = lastName;
-            cell4.textContent = mobile;
+            cell1.textContent = guest.id;
+            cell2.textContent = guest.firstName;
+            cell3.textContent = guest.lastName;
+            cell4.textContent = guest.mobile;
         // Clear input fields
-        
+        });
 
     document.getElementById("id").value = "";
     document.getElementById("firstName").value = "";
     document.getElementById("lastName").value = "";
     document.getElementById("mobile").value = "";
     document.getElementById("notes").value = "";
+
+    var newMeeting = {
+            meetingId : "1",
+            building: building,
+            floor: floor,
+            host: host,
+            guests: existingGuests
+        }
+
+    console.log(newMeeting);
+        // Get existing meetings or initialize an empty array
+        var existingMeetings = JSON.parse(localStorage.getItem("meetings")) || [];
+
+        // Add the new meeting details
+        existingMeetings.push(newMeeting);
+
+        // Save the updated meetings array to local storage
+        localStorage.setItem("meetings", JSON.stringify(existingMeetings));
     }
  
-
+    window.onload = loadGuestDetails;
   
 function cancelUpdate() {
     // סגור את חלונית ההוספה
@@ -94,12 +101,6 @@ function cancelUpdate() {
 function notifyUser() {
     alert("vhh");
 }
-
-window.addEventListener('beforeunload', function () {
-    // Clear the data from the "guests" and "meetings" databases in local storage
-    localStorage.removeItem("guests");
-    //localStorage.removeItem("meetings");
-});
 
 
 
