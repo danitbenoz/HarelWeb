0b9337838987158696e178b6820bd812d8f216b2
Danit <danit120@gmail.com>
2023-12-10 11:58:30 +0200

    Neat code

--- main.js
+++ main.js
@@ -1,108 +1,103 @@
 function toggleModal() {
     const guestModal = document.getElementById("guest-modal");
     guestModal.style.display = guestModal.style.display === "block" ? "none" : "block";
-  }
-  
+}
 
- 
-    function updateGuest() {
-        const id = document.getElementById("id").value;
-        const firstName = document.getElementById("firstName").value;
-        const lastName = document.getElementById("lastName").value;
-        const mobile = document.getElementById("mobile").value;
-        const notes = document.getElementById("notes").value;
-        const building = document.getElementById("building").value;
-        const floor = document.getElementById("floor").value;
-        const host = document.getElementById("host").value;
-    
-        var tableBody = document.getElementById("guest-table");
-        var detailsRow = document.getElementById("detailsRow");
-    
-        // If detailsRow already exists, remove it
-        if(id.length == 9 && mobile.length == 10) {
-   
-            var existingGuests = JSON.parse(localStorage.getItem("guests")) || [];
+
+
+function updateGuest() {
+    const id = document.getElementById("id").value;
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const mobile = document.getElementById("mobile").value;
+    const notes = document.getElementById("notes").value;
+    const building = document.getElementById("building").value;
+    const floor = document.getElementById("floor").value;
+    const host = document.getElementById("host").value;
+
+    var tableBody = document.getElementById("guest-table");
+    var detailsRow = document.getElementById("detailsRow");
+
+    // If detailsRow already exists, remove it
+    if (id.length == 9 && mobile.length == 10) {
+
+        var existingGuests = JSON.parse(localStorage.getItem("guests")) || [];
 
         // Add the new guest details
-            existingGuests.push({
-                id: id,
-                firstName: firstName,
-                lastName: lastName,
-                mobile: mobile
-            });
-            
-            localStorage.setItem("guests", JSON.stringify(existingGuests));
+        existingGuests.push({
+            id: id,
+            firstName: firstName,
+            lastName: lastName,
+            mobile: mobile
+        });
 
-            var newMeeting = {
-                meetingId : "1",
-                building: building,
-                floor: floor,
-                host: host,
-                guests: existingGuests
-            }
+        localStorage.setItem("guests", JSON.stringify(existingGuests));
 
-            console.log(newMeeting);
+        var newMeeting = {
+            meetingId: "1",
+            building: building,
+            floor: floor,
+            host: host,
+            guests: existingGuests
+        }
 
-            // Get existing meetings or initialize an empty array
-            var existingMeetings = JSON.parse(localStorage.getItem("meetings")) || [];
+        console.log(newMeeting);
 
-            // Add the new meeting details
-            existingMeetings.push(newMeeting);
+        // Get existing meetings or initialize an empty array
+        var existingMeetings = JSON.parse(localStorage.getItem("meetings")) || [];
 
-            // Save the updated meetings array to local storage
-            localStorage.setItem("meetings", JSON.stringify(existingMeetings));
+        // Add the new meeting details
+        existingMeetings.push(newMeeting);
+
+        // Save the updated meetings array to local storage
+        localStorage.setItem("meetings", JSON.stringify(existingMeetings));
 
 
-            var detailsRow = document.getElementById("detailsRow");
-            if (detailsRow) {
-                detailsRow.remove();
-            }
+        var detailsRow = document.getElementById("detailsRow");
+        if (detailsRow) {
+            detailsRow.remove();
+        }
 
-            var existingGuests = JSON.parse(localStorage.getItem("guests")) || [];
-            var tableBody = document.getElementById("guest-table");
-        
-    
-                var row = tableBody.insertRow();
-                var cell0 = row.insertCell(0);
-                var cell1 = row.insertCell(1);
-                var cell2 = row.insertCell(2);
-                var cell3 = row.insertCell(3);
-                var cell4 = row.insertCell(4);
+        var existingGuests = JSON.parse(localStorage.getItem("guests")) || [];
+        var tableBody = document.getElementById("guest-table");
 
-                cell0.textContent ="";
-                cell1.textContent = id;
-                cell2.textContent = firstName;
-                cell3.textContent = lastName;
-                cell4.textContent = mobile;
-            // Clear input fields
+
+        var row = tableBody.insertRow();
+        var cell0 = row.insertCell(0);
+        var cell1 = row.insertCell(1);
+        var cell2 = row.insertCell(2);
+        var cell3 = row.insertCell(3);
+        var cell4 = row.insertCell(4);
+
+        cell0.textContent = "";
+        cell1.textContent = id;
+        cell2.textContent = firstName;
+        cell3.textContent = lastName;
+        cell4.textContent = mobile;
     }
-    else 
-    {
-        if(id.length != 9 && mobile.length != 10)
-        {
+    else {
+        if (id.length != 9 && mobile.length != 10) {
             alert("מספר ת.ז ומספר טלפון אינם חוקיים")
         }
-        else if(id.length != 9)
-        {
+        else if (id.length != 9) {
             alert("מספר תז אינו חוקי")
         }
-        else{
+        else {
             alert("מספר טלפון אינו חוקי")
         }
     }
-        
+
 
     document.getElementById("id").value = "";
     document.getElementById("firstName").value = "";
     document.getElementById("lastName").value = "";
     document.getElementById("mobile").value = "";
     document.getElementById("notes").value = "";
-    }
- 
+}
 
-  
+
+
 function cancelUpdate() {
-    // סגור את חלונית ההוספה
     toggleModal();
 }
 
@@ -126,14 +121,13 @@ function validateIdInput(input) {
     }
 }
 
-function validatePhoneInput(input){
+function validatePhoneInput(input) {
     input.value = input.value.replace(/[^0-9]/g, '');
-    if(input.value.length > 10){
-        input.value = input.value.slice(0,10);
+    if (input.value.length > 10) {
+        input.value = input.value.slice(0, 10);
     }
 }
 
 
-  
 
-  
+
